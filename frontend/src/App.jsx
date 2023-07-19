611eff836eb5fa114e526080e45fba0c6a7756a8
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-07-19 11:08:29 +0200

    C4_GeoBoss added browser router and created main routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Game from "./pages/Game";
 import Results from "./pages/Results";
