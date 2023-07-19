89800830792ed4185ab081fe4f5270ea5d9a5a50
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-07-19 09:52:47 +0200

    C4_Geoboss database created

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,35 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- Suppression des tables existantes
+DROP TABLE IF EXISTS score;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+DROP TABLE IF EXISTS city;
+
+DROP TABLE IF EXISTS user;
+
+-- Création de la base de données
+CREATE DATABASE IF NOT EXISTS geoboss;
+
+USE geoboss;
+
+-- Table "user"
+CREATE TABLE IF NOT EXISTS user (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  pseudo VARCHAR(50),
+  hashedPassword VARCHAR(255)
+);
+
+-- Table "score"
+CREATE TABLE IF NOT EXISTS score (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  score INT,
+  FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
+-- Table "city"
+CREATE TABLE IF NOT EXISTS city (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  lat VARCHAR(50),
+  lon VARCHAR(50),
+  population INT
+);
